e38f933d72f3996354e894d5025ec3f69f2fe658
catamphetamine <kuchumovn@gmail.com>
2024-04-23 12:40:26 +0300

    .

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-// var proxy = require('cors-anywhere')
-var proxy = require('./lib/cors-anywhere')
+var corsProxy = require('cors-proxy-node')
 
-var config = require('./config.json')
-
-// Heroku: Listen on a specific host passed as the `HOST` environment variable.
-var host = process.env.HOST || config.host
-// Heroku: Listen on a specific port passed as the `PORT` environment variable.
-var port = process.env.PORT || config.port
-
-proxy.createServer({
-  originWhitelist: config.allowedRequestOrigins || [],
-  useCookies: config.cookies,
-  shareCookiesBetweenAllowedRequestOrigins: config.shareCookiesBetweenAllowedRequestOrigins,
-  requireHeader: ['origin', 'x-requested-with'] // The HTTP request must come from a web browser.
-}).listen(port, host, function() {
-  console.log('Running CORS proxy at http(s)://' + (host === '0.0.0.0' ? '<hostname>' : host) + ':' + port)
+corsProxy({
+  "host": "0.0.0.0",
+  "port": 8080,
+  "cookies": true,
+  "shareCookiesBetweenAllowedRequestOrigins": true,
+  "allowedRequestOrigins": [
+    "https://anychans.github.io",
+    "https://catamphetamine.github.io",
+    "https://captchan.surge.sh",
+    "http://localhost:1234"
+  ]
 })
