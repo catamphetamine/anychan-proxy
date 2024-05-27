88cea76b7761bc70c9c988e2b0c2b50fb2d1deb6
catamphetamine <kuchumovn@gmail.com>
2024-05-27 12:54:46 +0300

    .

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ corsProxy({
   "host": "0.0.0.0",
   "port": 8080,
   "cookies": true,
-  "shareCookiesBetweenAllowedRequestOrigins": true,
-  "allowedRequestOrigins": [
+  "shareCookiesBetweenOriginsInFromOriginWhitelist": true,
+  // "toOriginWhitelist": [
+  //   // 4chan CAPTCHA frame URL gets proxied in order to remove `Content-Security-Policy` HTTP response header.
+  //   // Proxying CAPTCHA frame URL results in `fromOrigin` being the origin of the proxy server itself.
+  //   // In order to not add the origin of the proxy server itself to `fromOriginWhitelist`,
+  //   // the CAPTCHA frame URL origin is added to `toOriginWhitelist` instead.
+  //   "https://sys.4chan.org"
+  // ],
+  "fromOriginWhitelist": [
     "https://anychans.github.io",
     "https://catamphetamine.github.io",
     "https://captchan.surge.sh",
